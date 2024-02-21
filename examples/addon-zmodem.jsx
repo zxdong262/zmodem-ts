9d17ba5bb204826e2c8d3be21bd52bf382d2666d
ZHAO Xudong <zxdong@gmail.com>
2024-02-21 12:10:21 +0800

    Edit readme

---
+++ examples/addon-zmodem.jsx
@@ -0,0 +1,58 @@
+import Sentry from 'zmodem-ts/dist/zsentry'
+// or
+// import Sentry from 'zmodem-ts/esm/zsentry.mjs'
+
+// a xtermjs addon example
+export class AddonZmodem {
+  _disposables = []
+
+  activate (terminal) {
+    terminal.zmodemAttach = this.zmodemAttach
+  }
+
+  sendWebSocket = (octets) => {
+    const { socket } = this
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      return socket.send(new Uint8Array(octets))
+    } else {
+      console.error('WebSocket is not open')
+    }
+  }
+
+  zmodemAttach = (ctx) => {
+    this.socket = ctx.socket
+    this.term = ctx.term
+    this.ctx = ctx
+    this.zsentry = new Sentry({
+      to_terminal: (octets) => {
+        if (ctx.onZmodem) {
+          this.term.write(String.fromCharCode.apply(String, octets))
+        }
+      },
+      sender: this.sendWebSocket,
+      on_retract: ctx.onzmodemRetract,
+      on_detect: ctx.onZmodemDetect
+    })
+    this.socket.binaryType = 'arraybuffer'
+    this.socket.addEventListener('message', this.handleWSMessage)
+  }
+
+  handleWSMessage = (evt) => {
+    if (typeof evt.data === 'string') {
+      if (this.ctx.onZmodem) {
+        this.term.write(evt.data)
+      }
+    } else {
+      this.zsentry.consume(evt.data)
+    }
+  }
+
+  dispose = () => {
+    this.socket && this.socket.removeEventListener('message', this.handleWSMessage)
+    this._disposables.forEach(d => d.dispose())
+    this._disposables.length = 0
+    this.term = null
+    this.zsentry = null
+    this.socket = null
+  }
+}
